f1b4dc5a76a132a11e168cb21f35fdb1db064d2e
unknown <basipraneethkumar2@gmail.com>
2026-03-21 19:47:33 +0530

    UC9:PalindromeChecker

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,34 +1,80 @@
 import java.util.*;
 
+// Palindrome Checker Class
+class PalindromeChecker {
+    private String str;
+
+    // Constructor
+    public PalindromeChecker(String str) {
+        this.str = str;
+    }
+
+    // Getter
+    public String getString() {
+        return str;
+    }
+
+    // Setter
+    public void setString(String str) {
+        this.str = str;
+    }
+
+    // Normalize string
+    private String normalize() {
+        return str.replaceAll("\\s+", "").toLowerCase();
+    }
+
+    // Two-pointer method
+    public boolean checkPalindrome() {
+        String s = normalize();
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    // Stack method
+    public boolean checkUsingStack() {
+        String s = normalize();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray())
+            stack.push(c);
+
+        for (char c : s.toCharArray()) {
+            if (c != stack.pop())
+                return false;
+        }
+        return true;
+    }
+}
+
+// Main Class (Renamed)
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
-        String str = sc.nextLine();
+        String input = sc.nextLine();
 
-        // Normalize: remove spaces & convert to lowercase
-        str = str.replaceAll("\\s+", "").toLowerCase();
+        PalindromeChecker checker = new PalindromeChecker(input);
 
-        int left = 0;
-        int right = str.length() - 1;
-        boolean isPalindrome = true;
-
-        // Two-pointer comparison
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                isPalindrome = false;
-                break;
-            }
-            left++;
-            right--;
-        }
-
-        // Output
-        if (isPalindrome)
-            System.out.println("Palindrome");
+        // Two-pointer result
+        if (checker.checkPalindrome())
+            System.out.println("Palindrome (Two Pointer)");
         else
-            System.out.println("Not Palindrome");
+            System.out.println("Not Palindrome (Two Pointer)");
+
+        // Stack result
+        if (checker.checkUsingStack())
+            System.out.println("Palindrome (Stack)");
+        else
+            System.out.println("Not Palindrome (Stack)");
 
         sc.close();
     }
